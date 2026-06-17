fd603066de08d6a699ac65addead72e657aebddf
Thiyaneshs <thiyaneshs.24it@kongu.edu>
2026-06-17 12:05:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://atm-application-7y7n.vercel.app/",
 }));
 
 app.use(express.json());
